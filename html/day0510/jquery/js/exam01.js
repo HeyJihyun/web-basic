e708b8126acab5ac6c54fa401fe507b867cf6647
Kim jihyun <j.hyun2658@gmail.com>
2023-05-10 14:49:57 +0900

    day0510 exam01.js 수정
    exam01.js 함수 정리

--- html/day0510/jquery/js/exam01.js
+++ html/day0510/jquery/js/exam01.js
@@ -1,16 +1,28 @@
 $(function () {
   $("#start").click(function () {
-    let text = null;
-    while (text === null || text.length === 0) {
-      text = prompt("텍스트를 입력해주세요");
-    }
-    $("#start").css("display", "none");
-    $(".text").html(text);
-    $(".buttonBox").css("display", "block");
+    // start 버튼 클릭 시 start 함수 실행
+    start();
   });
 
-  $(".buttonBox>button").click(function () {
-    const color = $(this).text();
-    $(".text").css("color", color);
+  $(".buttonBox > button").click(function () {
+    // 각 색상 버튼 클릭 시 chageColor 함수실행
+    chageColor($(this).text());
   });
 });
+
+// prompt로 텍스트 입력 받는 함수
+function start() {
+  let text = null;
+  while (text === null || text.length === 0) {
+    text = prompt("텍스트를 입력해주세요");
+  }
+  $("#start").css("display", "none");
+  $(".text").html(text);
+  $(".buttonBox").css("display", "block");
+}
+
+// 설정한 color의 색상으로 변경하는 함수
+function chageColor(color) {
+  $(".text").css("color", color);
+  alert(color + "색으로 변경되었습니다");
+}
